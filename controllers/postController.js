7676d40c033c2386eed652174832a97001e43e7b
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-05-30 10:46:35 +0700

    parse JSON response and add new endpoint

--- controllers/postController.js
+++ controllers/postController.js
@@ -19,44 +19,50 @@ const errorHandler = (err) => {
     return errors;
 };
 
-const getLocationData = (place_id) => {
+const getLocationData = async (place_id) => {
     var config = {
-      method: 'get',
-      url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name%2Ctypes%2Cformatted_address%2Ceditorial_summary&key=${process.env.GOOGLE_MAPS_API_KEY}&languages=id`,
-      headers: { }
+        method: 'get',
+        url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name%2Ctypes%2Cformatted_address%2Ceditorial_summary&key=${process.env.GOOGLE_MAPS_API_KEY}&languages=id`,
+        headers: {}
     };
 
     var photo = {
         method: 'get',
         url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=photos&key=${process.env.GOOGLE_MAPS_API_KEY}&languages=id`,
-        headers: { }
+        headers: {}
     };
-    
-    axios(config)
-    .then(function (response) {
-        console.log(JSON.stringify(response.data));
-    })
-    // axios(photo)
-    // .then(function (response) {
-    //     console.log(JSON.stringify(response.data));
-    // })
-    .catch(function (error) {
-        console.log(error);
-    });
+
+    return await axios(config)
+        .then(function (response) {
+            const { result } = response.data;
+            return result;
+        });
 };
 
-getLocationData("ChIJN1t_tDeuEmsRUsoyG83frY4");
-
-module.exports.get_list_location = async (req,res) => {
-    Location.find({}).then((postObject) => {
-        res.status(200).json(postObject);
-    }).catch((err) => res.status(400).json({ error: err.message, status: 400}));
+module.exports.get_list_location = async (req, res) => {
+    try {
+        let listLocation = [];
+        var myCursor = await Location.find({});
+        for (let i = 0; i < myCursor.length; i++) {
+            const result = await getLocationData(myCursor[i].gmapsID);
+            const { formatted_address, name, types } = result;
+            listLocation.push({
+                address: formatted_address,
+                name: name,
+                type: types,
+                GMapsID: myCursor[i].gmapsID
+            });
+        }
+        res.status(200).json({ status: 200, listLocation: listLocation });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 }
 
-module.exports.create_location = async (req,res) => {
+module.exports.create_location = async (req, res) => {
     const { gmapsID } = req.body;
     try {
-        const location = await Location.create({gmapsID});
+        const location = await Location.create({ gmapsID });
         res.status(200).json({ locationID: location._id });
     } catch (err) {
         const message = errorHandler(err);
@@ -64,15 +70,28 @@ module.exports.create_location = async (req,res) => {
     }
 }
 
-module.exports.create_comment = async (req,res) => {
+module.exports.create_comment = async (req, res) => {
     const { locationID, userID, starRating, comment } = req.body;
     try {
-        await User.findOneAndUpdate({_id:userID}, {$inc : {"comments": 1}});
-        const newComment = await Comment.create({userID, starRating, comment});
-        const location = await Location.findOneAndUpdate({_id:locationID}, {$push : {"commentsID": newComment._id}});
-        res.status(200).json({ locationID: location._id });
+        await User.findOneAndUpdate({ _id: userID }, { $inc: { "comments": 1 } });
+        const newComment = await Comment.create({ userID, starRating, comment });
+        const location = await Location.findOneAndUpdate({ _id: locationID }, { $push: { "commentsID": newComment._id } });
+        res.status(200).json({ GMapsID: location.gmapsID });
     } catch (err) {
         console.log(err)
         res.status(400).json({ error: err.message });
     }
 }
+
+module.exports.get_location = async (req, res) => {
+    const  GMapsID  = req.query.GMapsID;
+    try {
+        const location = await getLocationData(GMapsID);
+        const { formatted_address, name, types } = location;
+        res.status(200).json({ address: formatted_address, name: name, type: types });
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ error: err.message });
+    }
+}
+
--- routes/post.js
+++ routes/post.js
@@ -6,4 +6,5 @@ const router = Router();
 router.get('/get_list_location', postController.get_list_location);
 router.post('/create_location', postController.create_location);
 router.post('/create_comment', postController.create_comment);
+router.get('/get_location', postController.get_location);
 module.exports = router;
