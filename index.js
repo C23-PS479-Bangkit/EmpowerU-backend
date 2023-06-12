e4bb9af54658fcfebf4921f8050a8c71972cf9d9
Akbar Maliki <akbarmaliki109@gmail.com>
2023-06-12 20:58:04 +0700

    Autogenerate service key

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const cookieParser = require("cookie-parser");
 const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 require('dotenv').config();
+const fs = require('fs')
 
 
 const app = express();
@@ -23,6 +24,28 @@ mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
+    const serviceAccount = {
+        "type": "service_account",
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+        "universe_domain": "googleapis.com"
+    }
+    let jsonString = JSON.stringify(serviceAccount,null, ' ')
+    jsonString = jsonString.replaceAll('\\n', 'n');
+    fs.writeFile('./capstone-project-387306-60ec83c14618.json', jsonString, err => {
+        if (err) {
+            console.log('Error writing file!', err)
+        } else {
+            console.log('Successfully wrote service account file!')
+        }
+    })
     console.log(`Server is running on port: ${PORT}`);
 })
 app.get("*", checkUser);
